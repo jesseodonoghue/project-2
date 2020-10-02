df7d4d009c29896806e955ed2f8b8ce8ebeedf7f
Jesse O'Donoghue <jesseodonoghue@gmail.com>
2020-10-01 21:39:29 -0400

    update appController and apiRoutes

--- controllers/appController.js
+++ controllers/appController.js
@@ -6,6 +6,18 @@ module.exports = function (db) {
         res.json(dbDrinks);
       });
     },
+    // Get Order by Customer ID
+    getOrder: function (req, res) {
+      db.Order.findAll({ where: { UserId: req.params.id } }).then(function (dbOrder) {
+        res.json(dbOrder);
+      });
+    },
+    // Get Order Item by Order ID
+    getOrderItem: function (req, res) {
+      db.OrderItem.findAll({ where: { OrderId: req.params.id } }).then(function (dbOrderItem) {
+        res.json(dbOrderItem);
+      });
+    },
     // Create a new order
     createOrder: function (req, res) {
       db.Order.create({ UserId: req.params.id }).then(function (dbOrder) {
